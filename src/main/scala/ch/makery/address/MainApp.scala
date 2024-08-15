3d429762ac06d59fc281ecf35eb9ee09cd829cea
ShaorenC <91179333+ShaorenCheah@users.noreply.github.com>
2024-08-15 17:31:06 +0800

    Scene Builder Example Setup
    - Utilized practical example to setup environment

---
+++ src/main/scala/ch/makery/address/MainApp.scala
@@ -0,0 +1,30 @@
+package ch.makery.address
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.Includes._
+import scalafxml.core.{NoDependencyResolver, FXMLView, FXMLLoader}
+import javafx.{scene => jfxs}
+
+object MainApp extends JFXApp {
+  
+  val rootResource = getClass.getResource("view/RootLayout.fxml")
+  val loader = new FXMLLoader(rootResource, NoDependencyResolver)
+  loader.load();
+  val roots = loader.getRoot[jfxs.layout.BorderPane]
+  
+  stage = new PrimaryStage {
+    title = "Rhythm"
+    scene = new Scene {
+      root = roots
+    }
+  }
+  def showRhythmOverview() = {
+    val resource = getClass.getResource("view/RhythmOverview.fxml")
+    val loader = new FXMLLoader(resource, NoDependencyResolver)
+    loader.load();
+    val roots = loader.getRoot[jfxs.layout.AnchorPane]
+    this.roots.setCenter(roots)
+  } 
+  showRhythmOverview()
+}
